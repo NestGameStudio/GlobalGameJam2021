d39fe111f0cd96c5c9a7b4ecbf1233d95e0be2b1
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-30 20:47:00 -0300

    final scene

--- RogueLoros2/Assets/Scripts/SceneController.cs
+++ RogueLoros2/Assets/Scripts/SceneController.cs
@@ -8,6 +8,10 @@ public class SceneController : MonoBehaviour
     public void GoToMenu() {
         SceneManager.LoadScene("MenuScene");
     }
+    public void GoToEndScene()
+    {
+        SceneManager.LoadScene("FinalScene");
+    }
 
     public void GoToGame() {
         SceneManager.LoadScene("GameScene");
