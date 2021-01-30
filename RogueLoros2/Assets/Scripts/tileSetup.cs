69eb6f27dcb254e79226b616af021a53b98f98d9
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-30 00:25:15 -0300

    thingz

---
+++ RogueLoros2/Assets/Scripts/tileSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileSetup : MonoBehaviour
+{
+    public enum tileType 
+    {
+        side4,
+        side3,
+        side2
+    }
+    public tileType tipoTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
