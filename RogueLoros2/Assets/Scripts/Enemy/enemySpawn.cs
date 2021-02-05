c779105ad45711f5160e722e1867cf11b5f11bdc
Lucas Lima <lucaslimareis3311@gmail.com>
2021-02-05 15:32:43 -0300

    better randomness in enemy spawning

--- RogueLoros2/Assets/Scripts/Enemy/enemySpawn.cs
+++ RogueLoros2/Assets/Scripts/Enemy/enemySpawn.cs
@@ -8,8 +8,9 @@ public class enemySpawn : MonoBehaviour
 
     public void spawnInimigo(GameObject tileObject, int chance)
     {
-        int chanceCheck = Random.Range(0,101);
-        if(chance >= chanceCheck)
+        //int chanceCheck = Random.Range(0,101);
+        int chanceCheck = (int)RandomFromDistribution.RandomRangeNormalDistribution(0, 101, RandomFromDistribution.ConfidenceLevel_e._60);
+        if (chance >= chanceCheck)
         {
             //instanciar inimigo
             GameObject inimigoNovo = Instantiate(inimigo,transform.position,Quaternion.identity);
