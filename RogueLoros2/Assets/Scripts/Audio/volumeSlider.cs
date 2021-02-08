e066009dce448475e4138e5e7dc55d27ac1d1ebd
Lucas Lima <lucaslimareis3311@gmail.com>
2021-02-08 15:57:37 -0300

    audio slider

---
+++ RogueLoros2/Assets/Scripts/Audio/volumeSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class volumeSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("GameVolume", 0.75f);
+    }
+
+    public void SetLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("GameVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("GameVolume", sliderValue);
+    }
+}
+
+
