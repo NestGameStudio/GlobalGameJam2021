45902630ad48316d584762b56b0c9895d4b6290c
Danny <thedannyvegas@gmail.com>
2021-02-10 01:44:48 -0300

    Fixes na DeathFog e tentativa de DestroyTiles (desativado por não funcionar ainda)

---
+++ RogueLoros2/Assets/Scripts/DestroyTiles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTiles : MonoBehaviour
+{
+    // void OnTriggerEnter(Collider other){
+    //     Debug.Log("Death Fog entrou em um Trigger de tag " + other.tag);
+    //     if(other.tag != "Player"){
+    //         Destroy(other.gameObject);
+    //     }
+    // }
+}
